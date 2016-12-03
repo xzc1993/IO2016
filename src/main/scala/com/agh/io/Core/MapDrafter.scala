2a6b4cf462907b3903f2ef283ae71dfd7741208a
xzc1993@gmail.com <xxx@xxx.xxx>
2016-12-03 16:22:34 +0100

    Added method for drawing robot paths. Robot position is now in the center of red dot instead of its BBOX corner.

--- src/main/scala/com/agh/io/Core/MapDrafter.scala
+++ src/main/scala/com/agh/io/Core/MapDrafter.scala
@@ -2,20 +2,62 @@ package com.agh.io.Core
 
 import java.awt.geom._
 import java.awt.image.{AffineTransformOp, BufferedImage}
-import java.awt.{BasicStroke, Color}
+import java.awt.{BasicStroke, Color, Graphics2D}
 
 import com.agh.io.Map.Map
 
 class MapDrafter(map: Map) {
+
     def draw(position: Position): Unit = {
         var canvas = new BufferedImage(imageSize._1 + 5, imageSize._2 + 5, BufferedImage.TYPE_INT_RGB)
 
         val graphics = canvas.createGraphics()
+        graphics.setRenderingHint(java.awt.RenderingHints.KEY_ANTIALIASING, java.awt.RenderingHints.VALUE_ANTIALIAS_ON)
 
+        drawMap(canvas, graphics)
+        drawRobot(position, graphics)
+
+        val tx = AffineTransform.getScaleInstance(1, -1)
+        tx.translate(0, -canvas.getHeight(null))
+        val op = new AffineTransformOp(tx, AffineTransformOp.TYPE_NEAREST_NEIGHBOR)
+        canvas = op.filter(canvas, null)
+
+        graphics.dispose()
+        javax.imageio.ImageIO.write(canvas, "png", new java.io.File("map.png"))
+    }
+
+    def drawPath(positions: Set[Position]): Unit = {
+        var canvas = new BufferedImage(imageSize._1 + 5, imageSize._2 + 5, BufferedImage.TYPE_INT_RGB)
+
+        val graphics = canvas.createGraphics()
+        graphics.setRenderingHint(java.awt.RenderingHints.KEY_ANTIALIASING, java.awt.RenderingHints.VALUE_ANTIALIAS_ON)
+
+        drawMap(canvas, graphics)
+        graphics.setColor(Color.RED)
+        positions.reduce((positionA: Position, positionB: Position) => {
+            val x1 = (positionA.point.x / mapWidth) * imageWidth
+            val y1 = (positionA.point.y / mapHeight) * imageHeight
+            val x2 = (positionB.point.x / mapWidth) * imageWidth
+            val y2 = (positionB.point.y / mapHeight) * imageHeight
+            graphics.draw(new Line2D.Double(x1, y1, x2, y2))
+            positionB
+        })
+        positions.foreach(position => {
+            drawRobot(position, graphics)
+        })
+
+        val tx = AffineTransform.getScaleInstance(1, -1)
+        tx.translate(0, -canvas.getHeight(null))
+        val op = new AffineTransformOp(tx, AffineTransformOp.TYPE_NEAREST_NEIGHBOR)
+        canvas = op.filter(canvas, null)
+
+        graphics.dispose()
+        javax.imageio.ImageIO.write(canvas, "png", new java.io.File("map.png"))
+    }
+
+    private def drawMap(canvas: BufferedImage, graphics: Graphics2D) = {
         graphics.setColor(Color.WHITE)
         graphics.fillRect(0, 0, canvas.getWidth, canvas.getHeight)
-
-        graphics.setRenderingHint(java.awt.RenderingHints.KEY_ANTIALIASING, java.awt.RenderingHints.VALUE_ANTIALIAS_ON)
 
         graphics.setStroke(new BasicStroke())
         graphics.setColor(Color.BLUE)
@@ -26,18 +68,11 @@ class MapDrafter(map: Map) {
             val y2 = (wall.to.y / mapHeight) * imageHeight
             graphics.draw(new Line2D.Double(x1, y1, x2, y2))
         })
+    }
 
+    private def drawRobot(position: Position, graphics: Graphics2D) = {
         graphics.setColor(Color.RED)
-        graphics.fill(new Ellipse2D.Double((position.point.x / mapWidth) * imageWidth, (position.point.y / mapHeight) * imageHeight, 10.0, 10.0))
-
-        val tx = AffineTransform.getScaleInstance(1, -1)
-        tx.translate(0, -canvas.getHeight(null))
-        val op = new AffineTransformOp(tx, AffineTransformOp.TYPE_NEAREST_NEIGHBOR)
-        canvas = op.filter(canvas, null)
-
-        graphics.dispose()
-
-        javax.imageio.ImageIO.write(canvas, "png", new java.io.File("map.png"))
+        graphics.fill(new Ellipse2D.Double((position.point.x / mapWidth) * imageWidth - robotRadius/2, (position.point.y / mapHeight) * imageHeight - robotRadius/2, robotRadius, robotRadius))
     }
 
     private val mapWidth = map.getMapWidth
@@ -45,6 +80,7 @@ class MapDrafter(map: Map) {
     private val heightToWidthRatio = mapHeight / mapWidth
 
     private val imageWidth = 477
+    private val robotRadius = 10
     private val imageHeight = (imageWidth * heightToWidthRatio).toInt
     private val imageSize = (imageWidth, imageHeight)
 }
