a7bb25a6c0da88279c7676b334007641d1b6af88
xzc1993@gmail.com <xxx@xxx.xxx>
2016-12-04 23:05:27 +0100

    Fixed positions not drawn in correct order.

--- src/main/scala/com/agh/io/Main.scala
+++ src/main/scala/com/agh/io/Main.scala
@@ -4,27 +4,33 @@ import java.io.{File, PrintWriter}
 
 import com.agh.io.Configuration.CommandLineParser
 import com.agh.io.Core._
-import com.agh.io.Map.MapLoader
+import com.agh.io.Map.{MapLoader, Point}
 import com.agh.io.Sensor.SensorLoader
+
+import scala.io.Source
 
 object Main extends App {
     val configuration = new CommandLineParser().load(args)
     val map = new MapLoader(configuration.mapDataFile).load()
     val sensor = new SensorLoader(configuration.sensorDataFile).load()
     val mapDrafter = new MapDrafter(map)
-    var results = Set[RatedPosition]()
+    var results = List[RatedPosition]()
 
-    for(idx <- 0 to 0){
+
+//    val positions = Source.fromFile("results.txt").getLines().map( line => {
+//        val coordinates = line.split(",")
+//        new Position( new Point(coordinates(0).toDouble, coordinates(1).toDouble), 0.0)
+//    })
+//    mapDrafter.drawPath(positions)
+    val pw = new PrintWriter(new File("results.txt" ))
+    for(idx <- 0 to sensor.scans.length){
         println(s"Calculating position for reading ${idx}")
         val positionCalculator = new PositionCalculator(idx, configuration, map, sensor)
-        results += positionCalculator.run()
+        val result = positionCalculator.run()
+        results = results :+ result
+        pw.write("" + result.position.point.x + "," + result.position.point.y + "\n")
+        pw.flush()
+        mapDrafter.drawPath(results.map( x => x.position).iterator)
     }
-
-    val pw = new PrintWriter(new File("results.txt" ))
-    results.foreach( rp => {
-        pw.write("" + rp.position.point.x + "," + rp.position.point.y)
-    })
     pw.close
-
-    mapDrafter.drawPath(results.map( x => x.position))
 }
