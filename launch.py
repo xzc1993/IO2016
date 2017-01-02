a0276b04c33949ead15239f57f96723597247e73
xzc1993@gmail.com <xxx@xxx.xxx>
2017-01-02 20:40:40 +0100

    Reduced walltime.

--- launch.py
+++ launch.py
@@ -5,7 +5,7 @@ for idx in range(0, 40, 4):
         'qsub',
         '-l', 'nodes=1:ppn=12',
         '-A', 'intob2016',
-        '-l', 'walltime=2:00:00',
+        '-l', 'walltime=1:00:00',
         '-F', '"{0} {1}"'.format(str(idx), str(idx+4)),
         '/people/plgkzieba/IntObl2016/main.sh'])
     process.communicate()
