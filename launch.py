5ea8b9169fe7f5bd95558f7a20f8399aac3c373e
Piotr Góralczyk <piotr.a.goralczyk@gmail.com>
2017-01-21 18:06:50 +0100

    Adjust scripts

--- launch.py
+++ launch.py
@@ -1,12 +1,16 @@
 import subprocess
+import sys
 
-for idx in range(0, 40, 4):
+start = int(sys.argv[1])
+input_file_name = sys.argv[2]
+
+for idx in range(start, start + 40, 4):
     process = subprocess.Popen([
         'qsub',
         '-l', 'nodes=1:ppn=12',
         '-A', 'intob2016',
         '-l', 'walltime=1:00:00',
-        '-F', '"{0} {1}"'.format(str(idx), str(idx+4)),
+        '-F', '"{0} {1} {2}"'.format(str(idx), str(idx+4), input_file_name),
         '/people/plgpgoralczyk/IntObl2016/main.sh'])
     process.communicate()
 
